208c8bc17400574f6b0bd344884b88898cc2173c
Your Name <you@example.com>
2022-11-23 19:22:20 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class LinkedList {
+
+    public static void display() {
+
+        System.out.println("Welcome to Data Structure Program (Linked List)");
+    }
+}
